9ae165edfd84615cf4750afae4f479f09a893f20
dipalimagare <dipalimagare2026@gmail.com>
2021-09-03 09:10:57 +0530

    Displaying Welcome to Address Book Program

---
+++ src/main/java/Day_37_Workshop/Day_37_Workshop/AddressBookMain.java
@@ -0,0 +1,13 @@
+package Day_37_Workshop.Day_37_Workshop;
+
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program in AddressBookMain class on Master Branch");
+
+	}
+
+}
